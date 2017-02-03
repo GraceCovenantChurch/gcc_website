bb19bb18c28a499f5d1840d3829c0bca92524bf8
Jay Jung <Jay Jung>
2017-02-03 18:06:48 -0500

    polished

--- server/routes/mainAnnouncements.js
+++ server/routes/mainAnnouncements.js
@@ -4,18 +4,18 @@ var router = express.Router();
 var data = [
   {
     "pic" : "ma1.jpg",
-    "title" : "Announcement 1",
-    "description" : "hello world 1"
+    "title" : "Welcome",
+    "description" : "Sunday Service @ 11:15am – Meyerson Hall (34th and Walnut)"
   },
   {
     "pic" : "ma2.jpg",
-    "title" : "Announcement2",
-    "description" : "hello world 2"
+    "title" : "Sign Up For Family Groups",
+    "description" : "You haven't checked out GCC unless you've checked out our family groups >"
   },
   {
     "pic": "ma3.jpg",
-    "title" : "Announcement3",
-    "description" : "hello world 3"
+    "title" : "Crossroad Retreat",
+    "description" : "Click here to sign up for Crossroads Retreat!"
   }
 ]
 
