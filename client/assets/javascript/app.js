21f3a1762f5c4478d0a8ac3fc62686d0f9543116
Jay Jung <Jay Jung>
2017-02-17 14:03:50 -0500

    added some transitions

--- client/assets/javascript/app.js
+++ client/assets/javascript/app.js
@@ -2,7 +2,7 @@ angular.module('gccweb', ['ngRoute', 'ngResource', 'angular-inview'])
   .controller('documentController', function($scope) {
     $scope.elementInView = function(inview, inviewInfo) {
       if (inview) {
-        
+        inviewInfo.element.removeClass("fade");
       }
     }
   });
