a6322c754455ad21d116c84970f716f3709e8a09
Jay Jung <Jay Jung>
2017-03-01 18:55:40 -0500

    finished staff, started beliefs

--- client/assets/javascript/controllers/aboutController.js
+++ client/assets/javascript/controllers/aboutController.js
@@ -1,4 +1,7 @@
 angular.module('gccweb')
   .controller('staffController', function($scope, Staff) {
     $scope.staffList = Staff.query();
+  })
+  .controller('believeController', function($scope, Believe) {
+    $scope.believeList = Believe.query();
   });
--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var mainAnnouncement = require('./routes/mainAnnouncements.js');
 var smallAnnouncement = require('./routes/smallAnnouncements.js');
 var memoryVerse = require('./routes/memoryVerse.js');
 var staff = require('./routes/staff.js');
+var believe = require('./routes/believe.js');
 var app = express();
 
 app.use(express.static('client'));
@@ -10,6 +11,7 @@ app.use('/mainAnnouncement', mainAnnouncement);
 app.use('/smallAnnouncement', smallAnnouncement);
 app.use('/memoryVerse', memoryVerse);
 app.use('/staff', staff);
+app.use('/believe', believe);
 
 app.listen(8000, function() {
   console.log("Listening on port 8000");
