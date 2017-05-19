bf36202bbbb01f54b378caf31458253871006494
Jay Jung <jjung@ad.radiatemedia.com>
2017-05-19 13:23:42 -0400

    project files

--- client2/app/app.component.ts
+++ client2/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'gcc-web',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello world {{name}}</h1>`,
 })
 export class AppComponent  { name = 'Angular'; }
