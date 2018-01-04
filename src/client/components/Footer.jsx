409fe926a198e102f83f31b7fd7ece150969f235
Allen He <Balthazar@Allens-MacBook-Pro.local>
2018-01-04 17:12:20 -0500

    Updated footer to match invision

--- src/client/components/Footer.jsx
+++ src/client/components/Footer.jsx
@@ -7,15 +7,14 @@ class Footer extends Component {
     render () {
         return (
             <footer>
-                <Center vertical>
+                <Center vertical horizontal>
                 <div className="container footer">
-                    <p>&copy; Grace Covenant Church</p>
-
                     <div className="icons">
                         <a target="_blank" className="footerLinks" href="https://www.facebook.com/gracecovenant/"><i className="fa fa-facebook fa-2x"></i></a>
                         <a target="_blank" className="footerLinks" href="https://twitter.com/gccphiladelphia"><i className="fa fa-twitter fa-2x"></i></a>
                         <a target="_blank" className="footerLinks" href="https://www.instagram.com/gccphiladelphia/"><i className="fa fa-instagram fa-2x"></i></a>
                     </div>
+                    <p>&copy; Grace Covenant Church</p>
                 </div>
                 </Center>
             </footer>
