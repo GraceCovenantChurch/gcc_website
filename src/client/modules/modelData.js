2c745042de5f31e0c59b1008aad8c3b5e5c02777
Allen He <Balthazar@Allens-MacBook-Pro.local>
2018-01-22 15:44:49 -0500

    Removed DOCUMENT stuff

--- src/client/modules/modelData.js
+++ src/client/modules/modelData.js
@@ -2,9 +2,6 @@ import fetch from 'isomorphic-fetch';
 import pluralize from 'pluralize';
 
 export const MODEL_DATA_LOADED = 'MODEL_DATA_LOADED';
-export const DOCUMENT_CREATED = 'DOCUMENT_CREATED';
-export const DOCUMENT_UPDATED = 'DOCUMENT_UPDATED';
-export const DOCUMENT_DELETED = 'DOCUMENT_DELETED';
 
 const initialState = {};
 const initialModelState = {
@@ -27,46 +24,6 @@ function model(state = initialModelState, action) {
         __DB__: DB,
         ids: action.data.map(datum => datum._id),
       });
-
-    case DOCUMENT_CREATED:
-      return Object.assign({}, state, {
-        __DB__: Object.assign({}, state.__DB__, {
-          [action.data._id]: action.data,
-        }),
-        ids: [...state.ids, action.data._id],
-      });
-
-    case DOCUMENT_UPDATED:
-      return Object.assign({}, state, {
-        __DB__: Object.assign({}, state.__DB__, {
-          [action.id]: action.data,
-        }),
-      });
-
-    case DOCUMENT_DELETED:
-      return Object.assign({}, state, {
-        __DB__: Object.assign({}, state.__DB__, {
-          [action.id]: undefined,
-        }),
-        ids: state.ids.filter(id => id !== action.id),
-      });
-
-    default:
-      return state;
-  }
-}
-
-export default function page(state = initialState, action) {
-  switch (action.type) {
-    case MODEL_DATA_LOADED:
-    case DOCUMENT_CREATED:
-    case DOCUMENT_UPDATED:
-    case DOCUMENT_DELETED: {
-      const key = pluralize(action.modelName);
-      return Object.assign({}, state, {
-        [key]: model(state[key], action),
-      });
-    }
 
     default:
       return state;
