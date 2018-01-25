97c4a3a2bac9e1937ec2c3d600cd4bffe9a9ff02
Allen He <Balthazar@Allens-MacBook-Pro.local>
2018-01-25 00:23:00 -0500

    Fixed linting errors

--- src/server/api/Events.js
+++ src/server/api/Events.js
@@ -4,11 +4,9 @@ import EventModel from '../../models/event';
 const router = Router();
 
 router.get('/', (req, res, next) => {
-  EventModel.find({
-  	published: true
-  }).sort({
-  	'startDate': 1
-  }).limit(3)
+  EventModel.find({ published: true })
+    .sort({ startDate: 1 })
+    .limit(3)
     .then(results => res.json(results))
     .catch(err => next(err));
 });
