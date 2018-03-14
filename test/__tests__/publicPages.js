f2149fe0cfeff454f2b65dc55025a10f1be13be3
Austin Eng <213reeses@gmail.com>
2018-03-14 16:35:39 -0400

    separate backend and frontend tests

--- test/__tests__/publicPages.js
+++ test/__tests__/publicPages.js
@@ -1,49 +1,31 @@
 /* eslint-env node, jest */
+/* global publicServer */
 
 import request from 'supertest-as-promised';
-import Helmet from 'react-helmet';
 import pretty from 'pretty';
 
-let server;
-let db;
-const initServer = require('../build/server/app').default.then((obj) => {
-  server = obj.server;
-  db = obj.db;
-});
-
-describe('Client Pages', () => {
-  beforeAll(() => {
-    Helmet.canUseDOM = false;
-    return initServer;
-  });
-
+describe('Public ServerTest', () => {
   it('renders Home page', () =>
-    request(server)
+    request(publicServer)
       .get('/')
       .expect(200)
       .then(res => expect(pretty(res.text)).toMatchSnapshot()));
 
   it('renders Welcome page', () =>
-    request(server)
+    request(publicServer)
       .get('/welcome')
       .expect(200)
       .then(res => expect(pretty(res.text)).toMatchSnapshot()));
 
   it('renders Family Group page', () =>
-    request(server)
+    request(publicServer)
       .get('/familygroup')
       .expect(200)
       .then(res => expect(pretty(res.text)).toMatchSnapshot()));
 
   it('renders 404 page', () =>
-    request(server)
+    request(publicServer)
       .get('/invalid')
       .expect(200)
       .then(res => expect(pretty(res.text)).toMatchSnapshot()));
-
-  afterAll(() => {
-    Helmet.canUseDOM = true;
-    server.close();
-    db.close();
-  });
 });
