1eee49da5b372b433d8ef213c57240fc14f097d6
Allen He <allenhe97@yahoo.com>
2018-03-31 14:12:20 -0400

    Added static data, setup box components, grid layout

--- src/client/pages/Staff.jsx
+++ src/client/pages/Staff.jsx
@@ -4,12 +4,67 @@ import Helmet from 'react-helmet';
 
 import withTitle from '../hoc/withTitle';
 import TitleBanner from '../components/TitleBanner';
+import Center from '../components/Center';
 
 import styles from './Staff.css';
+
+
+class StaffBox extends Component {
+  render(){
+    return(
+      <div className={styles.staffBox}>
+        <Center horizontal vertical>{this.props.name}</Center>
+      </div>
+    );
+  }
+}
+
+StaffBox.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  imageURL: PropTypes.string.isRequired,
+};
 
 class Staff extends Component {
   
   render() {
+    const staffList = [
+      {
+        name: 'Pastor Young Kim',
+        title: 'Senior Pastor',
+        imageURL: ''
+      },
+      {
+        name: 'Pastor Kirt Thallman',
+        title: 'University City Site Pastor',
+        imageURL: ''
+      },
+      {
+        name: 'Chris Chen',
+        title: 'University City College Staff',
+        imageURL: ''
+      },
+      {
+        name: 'Bryan Mun',
+        title: 'University City College Staff',
+        imageURL: ''
+      },
+      {
+        name: 'Josh Kwon',
+        title: 'Mainline College and Young Adult Staff',
+        imageURL: ''
+      },
+      {
+        name: 'Bekah Lee',
+        title: 'Mainline Children\'s Pastor',
+        imageURL: ''
+      },
+      {
+        name: 'Chris Kwon',
+        title: 'University City Worship Leader',
+        imageURL: ''
+      }
+    ]
 
     return (
       <div>
@@ -22,7 +77,13 @@ class Staff extends Component {
         </TitleBanner>
 
         <div className={styles.pageContent}>
-          
+          {staffList.map(staffObj => (
+            <StaffBox
+            name={staffObj.name}
+            title={staffObj.title}
+            imageURL={staffObj.imageURL}
+            />
+          ))}
         </div>
       </div>
     );
