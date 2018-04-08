3fd984267b5df2a4d6b2ff86b4e8e0fb4e3b8078
joeyzia <joey.zia@gmail.com>
2018-04-08 09:32:01 -0400

    npm doesn't exist as a command in CodeDeploy
    Found a fix and attempting: https://stackoverflow.com/questions/34032751/npm-issue-deploying-a-nodejs-instance-using-aws-codedeploy

--- scripts/deploy/npm-install.sh
+++ scripts/deploy/npm-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /home/ec2-user/.bash_profile
 cd /home/gcc_website
 npm run babel
 npm run webpack
--- scripts/deploy/npm-start.sh
+++ scripts/deploy/npm-start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+source /home/ec2-user/.bash_profile
 cd /home/gcc_website
 npm run serve
