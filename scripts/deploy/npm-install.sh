a8afaa87809e97894552b3372e0cc8a1fd286fd1
joeyzia <joey.zia@gmail.com>
2018-04-08 09:46:27 -0400

    Revert "npm doesn't exist as a command in CodeDeploy"
    This reverts commit 3fd984267b5df2a4d6b2ff86b4e8e0fb4e3b8078.

--- scripts/deploy/npm-install.sh
+++ scripts/deploy/npm-install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source /home/ec2-user/.bash_profile
 cd /home/gcc_website
 npm run babel
 npm run webpack
--- scripts/deploy/npm-start.sh
+++ scripts/deploy/npm-start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-source /home/ec2-user/.bash_profile
 cd /home/gcc_website
 npm run serve
