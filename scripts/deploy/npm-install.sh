7767408868018c9cfb1f4c928ff05484bb63107b
joeyzia <joey.zia@gmail.com>
2018-04-14 13:30:05 -0400

    fixed deploy scripts

--- scripts/deploy/npm-install.sh
+++ scripts/deploy/npm-install.sh
@@ -1,5 +1,6 @@
 source /home/ec2-user/.bash_profile
-cd /home/gcc_website
+cd /home/ec2-user/gcc_website
+npm install
 npm run babel
 npm run webpack
 npm run test
--- scripts/deploy/npm-start.sh
+++ scripts/deploy/npm-start.sh
@@ -1,3 +1,3 @@
 source /home/ec2-user/.bash_profile
-cd /home/gcc_website
+cd /home/ec2-user/gcc_website
 npm run serve
