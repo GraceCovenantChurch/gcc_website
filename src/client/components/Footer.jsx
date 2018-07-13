cd202f7124c291ccbe44d885145696a3b901bbbf
joeyzia <joey.zia@gmail.com>
2018-07-13 17:46:58 -0400

    CSS Tyling and div manipulation

--- src/client/components/Footer.jsx
+++ src/client/components/Footer.jsx
@@ -5,13 +5,17 @@ import styles from './Footer.css';
 const Footer = () => (
   <footer>
     <Center vertical horizontal>
-      <div className="container footer">
+    <div className={styles.footerBorder}>
+      <div className={styles.footerContainer + " container footer"}>
+        <div className={styles.copyright}>
+          <p>&copy; Grace Covenant Church</p>
+        </div>
         <div className={styles.icons}>
           <a target="_blank" rel="noopener noreferrer" className={styles.footerLinks} href="https://www.facebook.com/gracecovenant/"><i className="fa fa-facebook fa-2x" /></a>
           <a target="_blank" rel="noopener noreferrer" className={styles.footerLinks} href="https://twitter.com/gccphiladelphia"><i className="fa fa-twitter fa-2x" /></a>
           <a target="_blank" rel="noopener noreferrer" className={styles.footerLinks} href="https://www.instagram.com/gccphiladelphia/"><i className="fa fa-instagram fa-2x" /></a>
         </div>
-        <p>&copy; Grace Covenant Church</p>
+      </div>
       </div>
     </Center>
   </footer>
