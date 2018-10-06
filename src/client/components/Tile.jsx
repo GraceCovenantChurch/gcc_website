81c3ed4643b5e4bfbdd2c9ca3d14e1c06ca5bb97
Allen He <allenhe97@yahoo.com>
2018-10-06 17:02:30 -0400

    Rebase

--- src/client/components/Tile.jsx
+++ src/client/components/Tile.jsx
@@ -3,22 +3,6 @@ import PropTypes from 'prop-types';
 
 import styles from './Tile.css';
 
-<<<<<<< HEAD
-const materialStyles = {
-  card: {
-    minHeight: 850,
-    maxHeight: 1500,
-    maxWidth: 350,
-  },
-  media: {
-    height: 350,
-    margin: 'auto',
-    paddingTop: '56.25%',
-  },
-};
-
-=======
->>>>>>> Major Tiledeck rehaul
 const Tile = props => (
   <div className={styles.card}>
     <div className={styles.cardMedia}>
