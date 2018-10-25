9963783b129d45ddbabb2451717364341c065626
joeyzia <joey.zia@gmail.com>
2018-10-24 20:46:44 -0400

    Major Rework (#86)
    Implemented Chaereen's recommended changes.

--- src/client/components/Footer.jsx
+++ src/client/components/Footer.jsx
@@ -14,8 +14,8 @@ const Footer = () => (
           </div>
           <div className={styles.icons}>
             <a target="_blank" rel="noopener noreferrer" className={styles.footerLinks} href="https://www.facebook.com/gracecovenant/"><i className="fa fa-facebook fa-2x" /></a>
-            <a target="_blank" rel="noopener noreferrer" className={styles.footerLinks} href="https://twitter.com/gccphiladelphia"><i className="fa fa-twitter fa-2x" /></a>
             <a target="_blank" rel="noopener noreferrer" className={styles.footerLinks} href="https://www.instagram.com/gccphiladelphia/"><i className="fa fa-instagram fa-2x" /></a>
+            <a target="_blank" rel="noopener noreferrer" className={styles.footerLinks} href="https://twitter.com/gccphiladelphia"><i className="fa fa-twitter fa-2x" /></a>
           </div>
         </div>
       </div>
